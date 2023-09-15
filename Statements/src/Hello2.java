2d3af2cc277e28f686726aa7255995719d54f540
K6-Rakshith <rk.rakshith6@gmail.com>
2023-09-15 18:44:28 +0530

    Statements

---
+++ Statements/src/Hello2.java
@@ -0,0 +1,12 @@
+public class Hello2{
+	public static void main(String[] args)
+	{
+		int x=10, y=11;
+		if(!(x<y)||(x==y))
+		{
+			System.out.println("Condition is true");
+		}
+		else
+			System.out.println("Condition is false");
+	}
+}
---
+++ Statements/src/assign.java
@@ -0,0 +1,7 @@
+public class assign {
+    public static void main(String[] args) {
+        int x = 10; // Assign a value to x before using it
+        x = x % 5; // Now you can perform the operation
+        System.out.println(x);
+    }
+}
