eff9a89f50057b55ddcced687aaa9533c80ccfa6
K6-Rakshith <rk.rakshith6@gmail.com>
2023-10-21 14:56:12 +0530

    Packages

---
+++ Packages/src/engine/Ignition.java
@@ -0,0 +1,10 @@
+package engine;
+
+public class Ignition {
+	 public static void main(String args[]){  
+		   engine.piston obj = new engine.piston();//using fully qualified name  
+		   System.out.println(obj);  
+		  }  
+
+}
+
---
+++ Packages/src/engine/piston.java
@@ -0,0 +1,12 @@
+package engine;
+
+public class piston {
+	public static void main(String[] args)
+	{
+		String a;
+		a = "Let the piston begin";
+		System.out.println(a);    
+	}	  
+
+} 
+
